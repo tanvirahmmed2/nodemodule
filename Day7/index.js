ec85f143bc895c2ee762af83de7f94b87c0d7aee
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-07-16 02:57:41 +0600

    update

--- Day7/index.js
+++ Day7/index.js
@@ -1,12 +1,33 @@
-const http=require('http')
-const port= 3000
-const hostName= '127.0.0.1'
+const http = require('http')
+const fs = require('fs')
+const port = 3000
+const hostName = '192.168.101.227'
 
-const server= http.createServer((req, res)=>{
-    res.end("Welcome to the server")
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        fs.readFile('index.html', (err, data) => {
+            res.writeHead(200, { "content-type": 'text/html' })
+            res.write(data)
+            res.end()
+        })
+    }
+    else if (req.url === '/contact') {
+        fs.readFile('contact.html', (err, data) => {
+            res.writeHead(200, { "content-type": 'text/html' })
+            res.write(data)
+            res.end()
+        })
+    }
+    else {
+        fs.readFile('err.html', (err, data) => {
+            res.writeHead(400, { "content-type": 'text/html' })
+            res.write(data)
+            res.end()
+        })
+    }
 
 })
-server.listen(port, hostName, ()=>{
+server.listen(port, hostName, () => {
     console.log(`The server is runing in http://${hostName}:${port}`);
-    
+
 })
